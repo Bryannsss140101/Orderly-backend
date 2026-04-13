d9995067daad57f306bb7c38bf81a352cde1259a
Bryannsss140101 <bninamango.personal@gmail.com>
2026-04-13 14:39:31 -0500

    Initial commit

---
+++ src/main/java/com/project/ordearly/OrdearlyApplication.java
@@ -0,0 +1,13 @@
+package com.project.ordearly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdearlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdearlyApplication.class, args);
+	}
+
+}
